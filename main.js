793201f9cca08c441000da403d4ee58bf26a3ee5
Lance J Hollingsworth <lance.hollingsworth@aggiemail.usu.edu>
2020-11-07 19:08:14 -0700

    template

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
 "use strict";
 
+const homeController = require("./controllers/homeController");
+
 const express = require("express"),
   app = express(),
   layouts = require("express-ejs-layouts"),
+  router = require("./routes/index"),
+  methodOverride = require("method-override"),
   mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  "mongodb+srv://lanceworth15:cactusjack@module3.m2r86.mongodb.net/recipe_db1816?retryWrites=true&w=majority",
-  { useNewUrlParser: true }
+  "mongodb+srv://lanceworth15:cactusjack@module3.m2r86.mongodb.net/project?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
 );
-mongoose.set("useCreateIndex", true);
 
 const db = mongoose.connection;
 
-//this is a test
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
+});
+
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(layouts);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]
+  })
+);
+
+app.use(express.json());
+
+app.get("/", homeController.index);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
