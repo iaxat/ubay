dbaf2bece10994b5ad3d7a738ce4c96e84f56388
chetan16994 <chetan16994@gmail.com>
2020-12-15 22:23:03 -0700

    api

--- controllers/biddingController.js
+++ controllers/biddingController.js
@@ -12,7 +12,8 @@ module.exports = {
           let prod_id = prod._id;
           let a = new Date();
           a = a.getTime();
-          let rem_time = (prod.time - a) / (1000 * 60 * 60);
+          // let rem_time = (prod.time - a) / (1000 * 60 * 60);
+          let rem_time = 0;
           Product.findByIdAndUpdate(prod_id, {
             $set:
               { remainingTime: rem_time }
@@ -80,7 +81,6 @@ module.exports = {
       category: req.body.category,
       forBidding: "true",
       imageUrl: sampleFile1.name,
-      // status:"approved",
     };
     Product.create(productParams)
       .then(product => {
@@ -98,16 +98,19 @@ module.exports = {
     let prodId=req.params.id;
     let prod_currentPrice=req.params.id2;
     try {
+      if(prodId.remainingTime>0){
       Product.findByIdAndUpdate(prodId,{$set: {currentPrice: prod_currentPrice*1.10 , user_id_bid : req.user._id, username_bid:req.user.username}
     }).then(k => {
       res.locals.redirect = "/bidding";
       next();
     })
+  }else{
+    res.render("bidding/bid-expire");
+  }
     } catch (error) {
       next(error);
     }
   }else{
-    res.locals.redirect = "/bidding/login-message";
     res.render("bidding/login-message");
   }
   },
