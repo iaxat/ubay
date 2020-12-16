24ca9a64616fb2a40e0886b006a6b97e7de2ce32
Lance Hollingsworth <lance.hollingsworth6@gmail.com>
2020-12-15 22:26:37 -0700

    small change

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ mongoose.Promise = global.Promise;
 
 //Database connection
 mongoose.connect(
-  process.env.MONGO_URI
+  process.env.MONGO_URI,
   { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true , useFindAndModify: false}
 );
 
