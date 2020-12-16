9b3e6bbc33828a113ef4f6a9bd3ac3dd67538430
Lance Hollingsworth <lance.hollingsworth6@gmail.com>
2020-12-15 22:27:23 -0700

    small change

--- controllers/biddingController.js
+++ controllers/biddingController.js
@@ -104,12 +104,7 @@ module.exports = {
     let prod_currentPrice=req.params.id2;
     let newPrice = (prod_currentPrice * 1.10).toFixed(2);
     try {
-<<<<<<< HEAD
-      if(prodId.remainingTime>0){
-      Product.findByIdAndUpdate(prodId,{$set: {currentPrice: prod_currentPrice*1.10 , user_id_bid : req.user._id, username_bid:req.user.username}
-=======
       Product.findByIdAndUpdate(prodId,{$set: {currentPrice: newPrice , user_id_bid : req.user._id, username_bid:req.user.username}
->>>>>>> 9cf4c48368a0d0209e9ec915084ee81eda4f44f5
     }).then(k => {
       res.locals.redirect = "/bidding";
       next();
